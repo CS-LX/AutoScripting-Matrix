23c0a05cbd627d4e1d5734d45d3ffc7a61f19304
LX.. <1659404657@qq.com>
2024-02-20 13:30:27 +0800

    加入矩阵解构器

---
+++ AutoScripting-Matrix/Blocks/Gates/ASMDecomposerBlock.cs
@@ -0,0 +1,82 @@
+using Engine;
+using Engine.Graphics;
+using System;
+
+namespace Game
+{
+    public class ASMDecomposerBlock : ASMRotateableMountedElectricElementBlock
+    {
+        public const int Index = 613;
+
+        public readonly ASMDecomposerInfo[] Infos = [
+            new ASMDecomposerInfo("矩阵TO解构器", "", "ASMatrixToTO", [ASMElectricConnectorDirection.Bottom], [ASMElectricConnectorDirection.Left, ASMElectricConnectorDirection.Right]),
+        ];
+
+        public Texture2D[] textures;
+
+        public ASMDecomposerBlock()
+            : base("Models/ASMDecomposer", "Decomposer", 0.5f)
+        {
+        }
+
+        public override void Initialize() {
+            base.Initialize();
+            textures = new Texture2D[Infos.Length];
+            for (int i = 0; i < Infos.Length; i++) {//获取所有门的材质
+                textures[i] = ContentManager.Get<Texture2D>($"Textures/Decomposers/{Infos[i].Texture}");
+            }
+        }
+
+        public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData) {
+            int type = GetType(Terrain.ExtractData(value));
+            BlocksManager.DrawMeshBlock(primitivesRenderer, m_standaloneBlockMesh, textures[type], color, 2f * size, ref matrix, environmentData);
+        }
+
+        public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
+        {
+            int num = Terrain.ExtractData(value) & 0x1F;
+            int type = GetType(Terrain.ExtractData(value));
+            generator.GenerateMeshVertices(this, x, y, z, m_blockMeshes[num], Color.White, null, geometry.GetGeometry(textures[type]).SubsetOpaque);
+            GenerateASMWireVertices(generator, value, x, y, z, GetFace(value), m_centerBoxSize, Vector2.Zero, geometry.SubsetOpaque);
+        }
+
+        public override string GetDisplayName(SubsystemTerrain subsystemTerrain, int value) => Infos[GetType(Terrain.ExtractData(value))].DisplayName;
+
+        public override string GetDescription(int value) => Infos[GetType(Terrain.ExtractData(value))].Description;
+
+        public override IEnumerable<int> GetCreativeValues() {
+            for (int i = 0; i < Infos.Length; i++) {
+                yield return Terrain.MakeBlockValue(Index, 0, SetType(0, i));
+            }
+        }
+
+        public override void GetDropValues(SubsystemTerrain subsystemTerrain, int oldValue, int newValue, int toolLevel, List<BlockDropValue> dropValues, out bool showDebris) {
+            int data = Terrain.ExtractData(oldValue);
+            dropValues.Add(new BlockDropValue { Value = Terrain.MakeBlockValue(Index, 0, SetType(0, GetType(data))), Count = 1 });
+            showDebris = true;
+        }
+
+        public override ASMElectricElement CreateElectricElement(SubsystemASMElectricity subsystemElectricity, int value, int x, int y, int z) => new ASMCalcGateElectricElement(subsystemElectricity, new CellFace(x, y, z, GetFace(value)), value);
+        public override ASMElectricConnectorType? GetConnectorType(SubsystemTerrain terrain, int value, int face, int connectorFace, int x, int y, int z)
+        {
+            int data = Terrain.ExtractData(value);
+            if (GetFace(value) == face)
+            {
+                ASMElectricConnectorDirection? connectorDirection = SubsystemASMElectricity.GetConnectorDirection(GetFace(value), GetRotation(data), connectorFace);
+                if (connectorDirection == ASMElectricConnectorDirection.Right || connectorDirection == ASMElectricConnectorDirection.Left)
+                {
+                    return ASMElectricConnectorType.Input;
+                }
+                if (connectorDirection == ASMElectricConnectorDirection.Top || connectorDirection == ASMElectricConnectorDirection.In)
+                {
+                    return ASMElectricConnectorType.Output;
+                }
+            }
+            return null;
+        }
+
+        public static int GetType(int data) => (data >> 5) & 15;
+
+        public static int SetType(int data, int type) => (data & -481) | ((type & 15) << 5);
+    }
+}
---
+++ AutoScripting-Matrix/Blocks/Gates/ASMDecomposerInfo.cs
@@ -0,0 +1,9 @@
+namespace Game {
+    public struct ASMDecomposerInfo(string displayName, string description, string texture, ASMElectricConnectorDirection[] inputDirections, ASMElectricConnectorDirection[] outputDirections) {
+        public string DisplayName = displayName;
+        public string Description = description;
+        public string Texture = texture;
+        public ASMElectricConnectorDirection[] InputDirections = inputDirections;
+        public ASMElectricConnectorDirection[] OutputDirections = outputDirections;
+    }
+}
