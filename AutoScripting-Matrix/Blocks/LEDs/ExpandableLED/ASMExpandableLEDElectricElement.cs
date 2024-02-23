8643ea0618c67893f81a388161f09917b1a3c08b
LX.. <1659404657@qq.com>
2024-02-23 23:42:45 +0800

    可拓展无缝数码LED：可获取连在一起的所有LED集合

--- AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDElectricElement.cs
+++ AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDElectricElement.cs
@@ -12,34 +12,30 @@ namespace Game {
         public override bool OnInteract(TerrainRaycastResult raycastResult, ComponentMiner componentMiner) {
             CellFace cellFace = CellFaces[0];
             Dictionary<CellFace, ASMExpandableLEDElectricElement> all = new();
-            GetConnectedElectricElements(cellFace.X, cellFace.Y, cellFace.Z, all);
+            GetConnectedElectricElements(cellFace.X, cellFace.Y, cellFace.Z, cellFace.Face, all);
             SubsystemElectricity.SubsystemAudio.PlaySound("Audio/Click", 1f, 0f, new Vector3(cellFace.X, cellFace.Y, cellFace.Z), 2f, autoDelay: true);
             return true;
         }
 
-        public void GetConnectedElectricElements(int x, int y, int z, Dictionary<CellFace, ASMExpandableLEDElectricElement> parent) {
+        public void GetConnectedElectricElements(int x, int y, int z, int face, Dictionary<CellFace, ASMExpandableLEDElectricElement> parent) {
 
-            if (parent.ContainsKey(new CellFace(x, y, z, 4))) return;//有本地的，重复了，直接返回
+            if (parent.ContainsKey(new CellFace(x, y, z, face))) return;//有本地的，重复了，直接返回
             //先获取本地的电路元件
             ASMExpandableLEDElectricElement electricElement = SubsystemElectricity.GetElectricElement(x, y, z, 4) as ASMExpandableLEDElectricElement;
-            parent.Add(new CellFace(x, y, z, 4), electricElement);
+            parent.Add(new CellFace(x, y, z, face), electricElement);
 
-            int blockID_X = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(x + 1, y, z));
-            int blockID_Z = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(x , y, z + 1));
-            int blockID_NX = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(x - 1, y, z));
-            int blockID_NZ = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(x, y, z - 1));
+            Point3 center = new(x, y, z);
+            Point3[] axes = new Point3[4];
 
-            if (blockID_X == ASMExpandableLEDBlock.Index) {//可以往x方向走
-                GetConnectedElectricElements(x + 1, y, z, parent);
-            }
-            if (blockID_Z == ASMExpandableLEDBlock.Index) {//可以往z方向走
-                GetConnectedElectricElements(x, y, z + 1, parent);
-            }
-            if (blockID_NX == ASMExpandableLEDBlock.Index) {//可以往-x方向走
-                GetConnectedElectricElements(x - 1, y, z, parent);
-            }
-            if (blockID_NZ == ASMExpandableLEDBlock.Index) {//可以往-z方向走
-                GetConnectedElectricElements(x, y, z - 1, parent);
+            ASMELEDUtils.FaceToAxesAndConner(face, out axes, out _);
+
+            for (int i = 0; i < 4; i++) {
+                Point3 checkPoint = center + axes[i];
+                int blockID = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(checkPoint.X, checkPoint.Y, checkPoint.Z));
+                if (blockID == ASMExpandableLEDBlock.Index) {
+                    //可以往x方向走
+                    GetConnectedElectricElements(checkPoint.X, checkPoint.Y, checkPoint.Z, face, parent);
+                }
             }
         }
     }
