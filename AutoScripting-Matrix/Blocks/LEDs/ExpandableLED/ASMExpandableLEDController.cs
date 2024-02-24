139f783f571c822a6793638ffd2925e179326296
LX.. <1659404657@qq.com>
2024-02-25 00:11:34 +0800

    可拓展无缝数码LED：显示电压联动

--- AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDController.cs
+++ AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDController.cs
@@ -1,8 +1,42 @@
+using Engine;
+
 namespace Game {
     public class ASMExpandableLEDController {
         /// <summary>
         /// 表示在内存中的位置（唯一ID）
         /// </summary>
-        public string ID => GetHashCode().ToString();
+        public int ID => GetHashCode();
+
+        /// <summary>
+        /// 所有控制的元件的数量
+        /// </summary>
+        public int ControlledCount => m_ledElements.Count;
+
+        /// <summary>
+        /// 所控制的所有LED元件以及坐标
+        /// </summary>
+        public Dictionary<CellFace, ASMExpandableLEDElectricElement> m_ledElements = new Dictionary<CellFace, ASMExpandableLEDElectricElement>();
+
+        public Matrix DisplayMatrix;
+
+        /// <summary>
+        /// 通过指定一个控制器所管辖的LED来实例化一个控制器（实例化中控制器会执行CollectCellsMatrix）
+        /// </summary>
+        /// <param name="ledElectricElements"></param>
+        public ASMExpandableLEDController(Dictionary<CellFace, ASMExpandableLEDElectricElement> ledElectricElements) {
+            m_ledElements = ledElectricElements;
+            CollectCellsMatrix();
+        }
+
+        /// <summary>
+        /// 重新获取管辖的所有LED方块的显示电压，并且加在一起作为最终的显示电压
+        /// </summary>
+        public void CollectCellsMatrix() {
+            DisplayMatrix = Matrix.Zero;
+            foreach (var ledElement in m_ledElements.Values) {
+                if(ledElement == null) continue;
+                DisplayMatrix += ledElement.OutputMatrixToController();
+            }
+        }
     }
 }
--- AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDElectricElement.cs
+++ AutoScripting-Matrix/Blocks/LEDs/ExpandableLED/ASMExpandableLEDElectricElement.cs
@@ -10,6 +10,9 @@ namespace Game {
         public ASMGlowCuboid m_glowCubiod;
         public ASMGlowText m_glowText;
 
+        public Matrix m_outputMatrix;
+        public Matrix OutputMatrixToController() => m_outputMatrix;
+
         public ASMExpandableLEDElectricElement(SubsystemASMElectricity subsystemElectricity, CellFace cellFace) : base(subsystemElectricity, cellFace) {
             m_subsystemTerrain = SubsystemElectricity.SubsystemTerrain;
             m_subsystemGlow = SubsystemElectricity.Project.FindSubsystem<SubsystemASMGlow>();
@@ -52,10 +55,20 @@ namespace Game {
 
 
         public override bool Simulate() {
-            Console.WriteLine(m_subsystemControllers.GetControllersCount());
+            Matrix voltage = m_outputMatrix;
+            m_outputMatrix = Matrix.Zero;
+            foreach (ASMElectricConnection connection in Connections)
+            {
+                if (connection.ConnectorType != ASMElectricConnectorType.Output && connection.NeighborConnectorType != 0)
+                {
+                    m_outputMatrix += connection.NeighborElectricElement.GetOutputVoltage(connection.NeighborConnectorFace);
+                }
+            }
+            if(m_outputMatrix != voltage) m_subsystemControllers.GetController(CellFaces[0])?.CollectCellsMatrix();//如果输入的矩阵值发生变化，则告诉控制器重新收集各LED的矩阵电压
+
             if (m_subsystemControllers.IsThereAController(CellFaces[0])) {
                 m_glowCubiod.m_color = Color.Yellow;
-                m_glowText.m_text = m_subsystemControllers.GetController(CellFaces[0]).ID;
+                m_glowText.m_text = m_subsystemControllers.GetController(CellFaces[0])?.ID + "\r\n" + m_subsystemControllers.GetController(CellFaces[0])?.ControlledCount + "\r\n" + m_subsystemControllers.GetController(CellFaces[0])?.DisplayMatrix;
             }
             else {
                 m_glowCubiod.m_color = Color.Transparent;
@@ -76,11 +89,10 @@ namespace Game {
             }
 
             //为新的板子添加新控制器实例与对应关系
-            m_subsystemControllers.AddControllerByPoints(all.Keys.ToArray(), new ASMExpandableLEDController());
+            m_subsystemControllers.AddControllerByPoints(all.Keys.ToArray(), new ASMExpandableLEDController(all));
         }
 
         public void GetConnectedElectricElements(int x, int y, int z, int face, Dictionary<CellFace, ASMExpandableLEDElectricElement> parent) {
-
             if (parent.ContainsKey(new CellFace(x, y, z, face))) return;//有本地的，重复了，直接返回
             //先获取本地的电路元件
             ASMExpandableLEDElectricElement electricElement = SubsystemElectricity.GetElectricElement(x, y, z, face) as ASMExpandableLEDElectricElement;
@@ -93,12 +105,14 @@ namespace Game {
 
             for (int i = 0; i < 4; i++) {
                 Point3 checkPoint = center + axes[i];
-                int blockID = Terrain.ExtractContents(m_subsystemTerrain.Terrain.GetCellValueFast(checkPoint.X, checkPoint.Y, checkPoint.Z));
-                if (blockID == ASMExpandableLEDBlock.Index) {
+                int blockValue = m_subsystemTerrain.Terrain.GetCellValueFast(checkPoint.X, checkPoint.Y, checkPoint.Z);
+                int blockID = Terrain.ExtractContents(blockValue);
+                if (blockID == ASMExpandableLEDBlock.Index && GetFace(blockValue) == face) {
                     //可以往某方向走
                     GetConnectedElectricElements(checkPoint.X, checkPoint.Y, checkPoint.Z, face, parent);
                 }
             }
+            int GetFace(int value) => (Terrain.ExtractData(value) >> 2) & 7;
         }
     }
 }
