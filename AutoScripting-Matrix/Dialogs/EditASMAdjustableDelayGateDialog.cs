e30b4dda97fa327352de94ba27d44f1f3b32d862
LX.. <1659404657@qq.com>
2024-03-03 22:26:22 +0800

    可调延迟版：延迟可以不同步长调整

--- AutoScripting-Matrix/Dialogs/EditASMAdjustableDelayGateDialog.cs
+++ AutoScripting-Matrix/Dialogs/EditASMAdjustableDelayGateDialog.cs
@@ -10,8 +10,12 @@ namespace Game
 		public SliderWidget m_delaySlider;
 
 		public ButtonWidget m_plusButton;
+		public ButtonWidget m_plus10Button;
+		public ButtonWidget m_plus100Button;
 
 		public ButtonWidget m_minusButton;
+		public ButtonWidget m_minus10Button;
+		public ButtonWidget m_minus100Button;
 
 		public LabelWidget m_delayLabel;
 
@@ -29,7 +33,11 @@ namespace Game
 			LoadContents(this, node);
 			m_delaySlider = Children.Find<SliderWidget>("EditAdjustableDelayGateDialog.DelaySlider");
 			m_plusButton = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.PlusButton");
+			m_plus10Button = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.Plus10Button");
+			m_plus100Button = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.Plus100Button");
 			m_minusButton = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.MinusButton");
+			m_minus10Button = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.Minus10Button");
+			m_minus100Button = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.Minus100Button");
 			m_delayLabel = Children.Find<LabelWidget>("EditAdjustableDelayGateDialog.Label");
 			m_okButton = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.OK");
 			m_cancelButton = Children.Find<ButtonWidget>("EditAdjustableDelayGateDialog.Cancel");
@@ -48,9 +56,25 @@ namespace Game
 			{
 				m_delay = MathUtils.Max(m_delay - 1, (int)m_delaySlider.MinValue);
 			}
+			if (m_minus10Button.IsClicked)
+			{
+				m_delay = MathUtils.Max(m_delay - 10, (int)m_delaySlider.MinValue);
+			}
+			if (m_minus100Button.IsClicked)
+			{
+				m_delay = MathUtils.Max(m_delay - 100, (int)m_delaySlider.MinValue);
+			}
 			if (m_plusButton.IsClicked)
 			{
 				m_delay = MathUtils.Min(m_delay + 1, (int)m_delaySlider.MaxValue);
+			}
+			if (m_plus10Button.IsClicked)
+			{
+				m_delay = MathUtils.Min(m_delay + 10, (int)m_delaySlider.MaxValue);
+			}
+			if (m_plus100Button.IsClicked)
+			{
+				m_delay = MathUtils.Min(m_delay + 100, (int)m_delaySlider.MaxValue);
 			}
 			if (m_okButton.IsClicked)
 			{
@@ -66,8 +90,8 @@ namespace Game
 		public void UpdateControls()
 		{
 			m_delaySlider.Value = m_delay;
-			m_minusButton.IsEnabled = m_delay > m_delaySlider.MinValue;
-			m_plusButton.IsEnabled = m_delay < m_delaySlider.MaxValue;
+			m_minusButton.IsEnabled = m_minus10Button.IsEnabled = m_minus100Button.IsEnabled = m_delay > m_delaySlider.MinValue;
+			m_plusButton.IsEnabled = m_plus10Button.IsEnabled = m_plus100Button.IsEnabled = m_delay < m_delaySlider.MaxValue;
 			m_delayLabel.Text = string.Format("延迟: " + Math.Round((m_delay + 1) * 0.01f, 2));
 		}
 
