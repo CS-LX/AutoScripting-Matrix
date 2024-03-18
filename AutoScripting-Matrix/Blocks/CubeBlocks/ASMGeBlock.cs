3d0df148defddc464071a42b3d902495efce50e0
LX.. <1659404657@qq.com>
2024-03-18 20:50:28 +0800

    完善矩阵变体锗块解释

--- AutoScripting-Matrix/Blocks/CubeBlocks/ASMGeBlock.cs
+++ AutoScripting-Matrix/Blocks/CubeBlocks/ASMGeBlock.cs
@@ -16,6 +16,8 @@ namespace Game {
 
         public override string GetDisplayName(SubsystemTerrain subsystemTerrain, int value) => "矩阵变体锗块";
 
+        public override string GetDescription(int value) => "更适合矩阵玩家与线代学者的锗块。\r\n可以用来作装饰，作电路载体。\r\n手持它时点击编辑按钮即可打开矩阵设置界面。";
+
         public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z)
         {
             generator.GenerateCubeVertices(this, value, x, y, z, Color.White, geometry.GetGeometry(texture).OpaqueSubsetsByFace);
