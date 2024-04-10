91e386928a5f035f009081a405ec79f907f80cf2
LX.. <1659404657@qq.com>
2024-04-10 12:30:32 +0800

    Gizmos：优化对话框

--- AutoScripting-Matrix/Dialogs/EditASMGizmosDialog.cs
+++ AutoScripting-Matrix/Dialogs/EditASMGizmosDialog.cs
@@ -4,28 +4,31 @@ using Engine;
 
 namespace Game {
     public class EditASMGizmosDialog : ASMDialog {
-        public ButtonWidget ignoreAllButtonWidget;
+        public ButtonWidget m_ignoreAllButtonWidget;
 
-        public ButtonWidget unignoreButtonWidget;
+        public ButtonWidget m_unignoreButtonWidget;
+
+        public ButtonWidget m_closeButtonWidget;
 
         public SubsystemASMGizmos m_subsystemAsmGizmos;
 
         public EditASMGizmosDialog(SubsystemASMGizmos subsystemAsmGizmos) : base("Dialogs/EditASMGizmosDialog") {
             m_subsystemAsmGizmos = subsystemAsmGizmos;
-            ignoreAllButtonWidget = Children.Find<ButtonWidget>("IgnoreAllButton");
-            unignoreButtonWidget = Children.Find<ButtonWidget>("UnignoreAllButton");
+            m_ignoreAllButtonWidget = Children.Find<ButtonWidget>("IgnoreAllButton");
+            m_unignoreButtonWidget = Children.Find<ButtonWidget>("UnignoreAllButton");
+            m_closeButtonWidget = Children.Find<ButtonWidget>("CloseButton");
         }
 
         public override void Update() {
-            if (ignoreAllButtonWidget.IsClicked) {
+            if (m_ignoreAllButtonWidget.IsClicked) {
                 m_subsystemAsmGizmos.IgnoreAll();
                 Dismiss();
             }
-            else if (unignoreButtonWidget.IsClicked) {
+            else if (m_unignoreButtonWidget.IsClicked) {
                 m_subsystemAsmGizmos.UnignoreAll();
                 Dismiss();
             }
-            else if(Input.Ok || Input.Cancel) {
+            else if(Input.Ok || Input.Cancel || m_closeButtonWidget.IsClicked) {
                 Dismiss();
             }
         }
