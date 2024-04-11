aeca68783a84f8338aa66a9ece7d5c4c670a7248
LX.. <1659404657@qq.com>
2024-04-11 23:37:16 +0800

    摄像机板：修复Gizmos绘制异常

--- AutoScripting-Matrix/Blocks/LEDs/CameraLED/ComponentASMComplexCameraLED.cs
+++ AutoScripting-Matrix/Blocks/LEDs/CameraLED/ComponentASMComplexCameraLED.cs
@@ -279,6 +279,7 @@ namespace Game {
         }
 
         public void TopMostGizmosDraw(FlatBatch3D flatBatch) {
+            if(m_camera == null) return;
             flatBatch.QueueLine(m_position + Vector3.One * 0.5f, m_camera.ViewPosition, Color.Pink, Color.Violet);
             flatBatch.QueueBoundingBox(new BoundingBox(m_camera.ViewPosition - Vector3.One * 0.2f, m_camera.ViewPosition + Vector3.One * 0.2f), Color.Yellow);
             flatBatch.QueueCoordinate(m_camera.ViewMatrix.Invert(), 0.4f);
