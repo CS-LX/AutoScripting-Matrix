e414617749b792a7505e14a77a4f40e68cd34e61
LX.. <1659404657@qq.com>
2024-05-15 12:58:47 +0800

    小修设置

--- AutoScripting-Matrix/Blocks/Others/SubsystemASMSettingsSetterBehavior.cs
+++ AutoScripting-Matrix/Blocks/Others/SubsystemASMSettingsSetterBehavior.cs
@@ -1,13 +1,20 @@
+using Engine;
+
 namespace Game {
     public class SubsystemASMSettingsSetterBehavior : SubsystemBlockBehavior {
         public override int[] HandledBlocks => [ASMSettingsSetterBlock.Index];
 
-        public override bool OnEditBlock(int x, int y, int z, int value, ComponentPlayer componentPlayer){
+        public override bool OnEditBlock(int x, int y, int z, int value, ComponentPlayer componentPlayer) {
             ScreensManager.SwitchScreen("ASMSettings");
             return true;
         }
 
         public override bool OnEditInventoryItem(IInventory inventory, int slotIndex, ComponentPlayer componentPlayer) {
+            ScreensManager.SwitchScreen("ASMSettings");
+            return true;
+        }
+
+        public override bool OnUse(Ray3 ray, ComponentMiner componentMiner) {
             ScreensManager.SwitchScreen("ASMSettings");
             return true;
         }
